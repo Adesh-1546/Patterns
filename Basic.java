1d0c6a66f3af94c59612d3ba75d60fa613a42eaf
Adesh-1546 <adeshpol1234@gmail.com>
2025-08-31 23:52:27 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,25 @@
+package com.demo;
+
+public class Basic {
+	
+	public static void main(String[] args) {
+		
+//		int n = 5 ;
+//		for (int i=0 ; i<=n ; i++) {
+//			System.out.print("*");
+//			
+//			System.out.println();
+//		}
+		
+		int n = 5;
+		int i = 0;
+		while(i<=n) {
+			System.out.print("*");
+			
+			System.out.println();
+			i++;
+		}
+		
+	}
+
+}
---
+++ InvertedHalfby180.java
@@ -0,0 +1,31 @@
+package com.demo;
+/*
+       *
+     * *
+   * * *
+ * * * *
+ 
+ 
+ */
+
+public class InvertedHalfby180 {
+	
+	public static void main(String[] args) {
+		int n = 5 ;
+		
+		for( int i=1 ; i<= n ; i++) {
+			
+			// space print
+			for(int j=1 ; j<=n-i; j++) {
+				System.out.print(" ");
+			}
+			
+			// star print
+			for (int j=1 ; j<=i ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
